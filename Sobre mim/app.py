655027e26a4cbba307e6d59f5504de7faaa75559
Roberto Marins <137070671+betorfm@users.noreply.github.com>
2024-12-21 18:40:47 -0300

    Add files via upload

---
+++ Sobre mim/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+# Configuração da conexão com o banco de dados
+db_config = {
+    "dbname": "cadastro",
+    "user": "postgres",
+    "password": "Pretinha123**",
+    "host": "localhost",
+    "port": "5432"
+}
+
+# Rota para exibir o formulário
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Rota para processar os dados enviados
+@app.route("/submit", methods=["POST"])
+def submit():
+    nome = request.form["name"]
+    email = request.form["email"]
+    telefone = request.form["telefone"]
+
+    try:
+        # Conectar ao banco de dados PostgreSQL
+        conn = psycopg2.connect(**db_config)
+        cur = conn.cursor()
+
+        # Inserir os dados no banco de dados
+        cur.execute("INSERT INTO usuarios (nome, email, telefone) VALUES (%s, %s, %s)", (nome, email, telefone))
+        conn.commit()
+
+        cur.close()
+        conn.close()
+        return "Dados enviados com sucesso!"
+    except Exception as e:
+        return f"Ocorreu um erro: {e}"
+
+# Iniciar o servidor
+if __name__ == "__main__":
+    app.run(debug=True)
